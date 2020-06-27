33789e4c1285f77dacaa431988ad6f3151e15b36
ezelking <timroberson@live.nl>
2020-06-27 22:49:18 +0200

    Expansiontile to show raid groups

--- flutter_app/lib/screens/raid_list_page.dart
+++ flutter_app/lib/screens/raid_list_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:pogo/alerts/report_raid_alert.dart';
+import 'package:pogo/models/raid_group.dart';
 import 'package:pogo/providers/raid_provider.dart';
 import 'package:provider/provider.dart';
 
@@ -13,68 +14,87 @@ class RaidListPage extends StatelessWidget {
       body: ListView.builder(
           itemCount: gyms.length,
           itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              leading: SizedBox(
-                width: 50,
-                child: Center(
-                  child: (gyms[index].raid != null)
-                      ? Image.asset(
-                          'assets/images/tier-${gyms[index].raid.tier}-egg.png',
-                          scale: 4,
-                        )
-                      : IconButton(
-                          icon: Icon(Icons.alarm_add),
-                          onPressed: () async {
-                            ReportRaidAlert.alert(context).then((value) => {
-                                  if (value != null)
-                                    {
-                                      Provider.of<RaidProvider>(context,
-                                              listen: false)
-                                          .reportRaid(
-                                              context, gyms[index].gymId, value)
-                                    }
-                                });
-                          },
+            return ExpansionTile(
+                leading: SizedBox(
+                  width: 50,
+                  child: Center(
+                    child: (gyms[index].raid != null)
+                        ? Image.asset(
+                            'assets/images/tier-${gyms[index].raid.tier}-egg.png',
+                            scale: 4,
+                          )
+                        : IconButton(
+                            icon: Icon(Icons.alarm_add),
+                            onPressed: () async {
+                              ReportRaidAlert.alert(context).then((value) => {
+                                    if (value != null)
+                                      {
+                                        Provider.of<RaidProvider>(context,
+                                                listen: false)
+                                            .reportRaid(context,
+                                                gyms[index].gymId, value)
+                                      }
+                                  });
+                            },
+                          ),
+                  ),
+                ),
+                title: Text(gyms[index].name),
+                trailing: SizedBox(
+                  width: 100,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: <Widget>[
+                      if (gyms[index].raid != null)
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            for (int i = 0; i < gyms[index].raid.tier; i++)
+                              Image.asset(
+                                'assets/images/raid-icon.png',
+                                scale: 4,
+                                color: Colors.black,
+                              ),
+                          ],
                         ),
+                      if (gyms[index].raid != null)
+                        RichText(
+                          text: TextSpan(
+                              style: TextStyle(
+                                  fontSize: 18, color: Colors.red[900]),
+                              text:
+                                  '${gyms[index].raid.startTime.hour.toString().padLeft(2, '0')}:${gyms[index].raid.startTime.minute.toString().padLeft(2, '0')}',
+                              children: [
+                                TextSpan(text: '-'),
+                                TextSpan(
+                                    text:
+                                        '${gyms[index].raid.endTime.hour.toString().padLeft(2, '0')}:${gyms[index].raid.endTime.minute.toString().padLeft(2, '0')}')
+                              ]),
+                        )
+                    ],
+                  ),
                 ),
-              ),
-              title: Text(gyms[index].name),
-              trailing: SizedBox(
-                width: 100,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    if (gyms[index].raid != null)
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          for (int i = 0; i < gyms[index].raid.tier; i++)
-                            Image.asset(
-                              'assets/images/raid-icon.png',
-                              scale: 4,
-                              color: Colors.black,
-                            ),
-                        ],
-                      ),
-                    if (gyms[index].raid != null)
-                      RichText(
-                        text: TextSpan(
-                            style:
-                                TextStyle(fontSize: 18, color: Colors.red[900]),
-                            text:
-                                '${gyms[index].raid.startTime.hour.toString().padLeft(2, '0')}:${gyms[index].raid.startTime.minute.toString().padLeft(2, '0')}',
-                            children: [
-                              TextSpan(text: '-'),
-                              TextSpan(
-                                  text:
-                                      '${gyms[index].raid.endTime.hour.toString().padLeft(2, '0')}:${gyms[index].raid.endTime.minute.toString().padLeft(2, '0')}')
-                            ]),
-                      )
-                  ],
-                ),
-              ),
-            );
+                children: _createChildren(gyms[index]?.raid?.groups ?? null));
           }),
     );
+  }
+
+  List<Widget> _createChildren(List<RaidGroup> groups) {
+    return List<Widget>.generate((groups == null) ? 0 : groups.length + 1,
+        (int index) {
+      return (index >= groups.length)
+          ? ListTile(
+              title: Text('Create New Raid Party'),
+              trailing: Icon(Icons.group_add),
+            )
+          : ListTile(
+              title: Text(
+                  '${groups[index].time.hour.toString().padLeft(2, '0')}:${groups[index].time.minute.toString().padLeft(2, '0')}'),
+              trailing: Tab(
+                icon: Icon(Icons.group),
+                text: groups[index].raiders.length.toString(),
+              ),
+            );
+    });
   }
 }
